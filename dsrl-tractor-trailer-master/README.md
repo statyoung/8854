5acecf58ce48d65dfef107993960d9aac69ee138
statyoung <130282947+statyoung@users.noreply.github.com>
2025-04-21 19:42:31 +0900

    Delete dsrl-tractor-trailer-master directory

--- dsrl-tractor-trailer-master/README.md
+++
@@ -1,2 +0,0 @@
-# dsrl-tractor-trailer
-This repository is for team project of 2024 spring SNU DSRL lecture.
--- dsrl-tractor-trailer-master/envs/__init__.py
+++
@@ -1,6 +0,0 @@
-from gymnasium.envs.registration import register
-
-register(
-    id='TractorTrailer-v0',
-    entry_point='envs.tractor_trailer:TractorTrailer',
-)
